5a338dbbea4cdc384d636bc9ec98ae149392e5a5
wangyuyang <thelmg@163.com>
2021-03-08 18:37:12 +0800

    展示

---
+++ html/主站开发/展台/data.js
@@ -0,0 +1,17 @@
+var listData = [
+    {
+        "title":"随机密码",
+        "description":"生成随机密码",
+        "url":"https://my.soulfree.cn/password/"
+    },
+    {
+        "title":"时钟-拟态玻璃",
+        "description":"",
+        "url":"https://my.soulfree.cn/3/"
+    },
+    {
+        "title":"我的博客",
+        "description":"",
+        "url":"https://www.soulfree.cn"
+    },
+]
