9928561fe98225482f12cf59305d495c427ea33c
甘霖 <thelmg@163.com>
2021-03-13 16:52:07 +0800

    1.0 show

--- html/主站开发/展台/data.js
+++ html/主站开发/展台/data.js
@@ -1,32 +1,37 @@
 var listData = [
     {
-        "title":"随机密码",
+        "title": "我的博客",
+        "description": "",
+        "url": "https://www.soulfree.cn",
+        "imgUrl":"./image/5.png"
+    },
+    {
+        "title":"随机密码生成",
         "description":"生成随机密码",
-        "url":"https://my.soulfree.cn/password/"
+        "url": "https://my.soulfree.cn/password/",
+        "imgUrl":"./image/2.png"
     },
     {
         "title":"时钟-拟态玻璃",
         "description":"",
-        "url":"https://my.soulfree.cn/3/"
+        "url": "https://my.soulfree.cn/3/",
+        "imgUrl":"./image/img.png"
     },
     {
         "title":"鼠标-景深跟随",
         "description":"",
-        "url":"https://my.soulfree.cn/5/"
+        "url": "https://my.soulfree.cn/5/",
+        "imgUrl":"./image/3.png"
     },
-    {
-        "title":"图片加载",
-        "description":"大图加载时从模糊到清晰",
-        "url":"https://my.soulfree.cn/4/"
-    },
-    {
-        "title": "我的博客",
-        "description": "",
-        "url": "https://www.soulfree.cn"
-    },
-    {
-        "title": "百度2021校招地址",
-        "description": "",
-        "url": "https://talent.baidu.com/external/baidu/campus.html#/jobList/0-1227-10002"
-    }
+    // {
+    //     "title":"图片加载",
+    //     "description":"大图加载时从模糊到清晰",
+    //     "url":"https://my.soulfree.cn/4/"
+    // },
+    
+    // {
+    //     "title": "百度2021校招地址",
+    //     "description": "",
+    //     "url": "https://talent.baidu.com/external/baidu/campus.html#/jobList/0-1227-10002"
+    // }
 ]
