fa64b3cba1a1bc81b63cefd6d96a36a0a114b9db
甘霖 <thelmg@163.com>
2021-03-20 10:53:25 +0800

    oled

--- esp8266/开发/main.py
+++ esp8266/开发/main.py
@@ -1,5 +1,4 @@
 import ntptime
-import utime
 import time
 import machine
 
@@ -7,43 +6,38 @@ from machine import I2C
 i2c=machine.I2C(-1, sda=machine.Pin(9), scl=machine.Pin(10), freq=400000)
 from ssd1306 import SSD1306_I2C
 oled = SSD1306_I2C(128, 64, i2c)
-IF_NTP=0
-def ntpsettime(IF_NTP):
-    try:
-        ntptime.time()
-        ntptime.settime()
-        rtc=machine.RTC()
-        tampon1=utime.time()
-        tampon2=tampon1+8*60*60
-        rtc.datetime ( utime.localtime(tampon2)[0:3] + (0,) + utime.localtime(tampon2)[3:6] + (0,))
-        IF_NTP=1
-    except:
-        print('XXX')
-        IF_NTP=0
-    return IF_NTP
-#time.localtime()
-#开始循环
-utime.sleep(3)
-IF_NTP=ntpsettime(IF_NTP)
-count=0
+ntptime.settime()
+time.sleep(3)
 def gTime():
-    (year, month, mday, hour, minute, second, weekday, yearday)=utime.localtime()
-    return str(year)+'-'+str(month)+'-'+str(mday)+' '+str(hour)+':'+str(minute)+':'+str(second)
+    (year, month, mday, hour, minute, second, weekday, yearday) = time.localtime()
+    hour = hour + 8
+    
+    return {
+        "day": (str(year) + '-' + str(month) + '-' + str(mday)),
+        "time": (str(hour) + ':' + str(minute) + ':' + str(second))
+    }
 
-def show_led(texts):
-    (year, month, mday, hour, minute, second, weekday, yearday) = utime.localtime()
-    if second < 10:
-        second = "0" + str(second)
-    if minute < 10:
-        minute ="0"+str(minute)
-    if month < 10:
-        month ="0"+str(month)
+def show_led():
     oled.fill(0)
-    oled.text(str(str(year) + '-' + str(month) + '-' + str(mday)), 25, 0)
-    oled.text(str(str(hour)+':'+str(minute)+':'+str(second)), 32, 8)
+    oled.text( gTime()["day"] , 25, 0)
+    oled.text( gTime()["time"], 32, 8)
     oled.show()
 
-
 while True:
-    show_led(gTime())
+    show_led()
     time.sleep(.1)
+
+
+
+
+def show_bit_image(self,image, x_pos, y_pos, size_x, size_y, inv=False):
+    x = 0
+    y = 0
+    for data in image:
+    for a in range(0, 31):
+        if (data & 1<<a):
+        oled.pixel(x_pos + x, y_pos + y, not inv)
+        x = x + 1
+        if x == size_x:
+        x = 0
+        y = y + 1
