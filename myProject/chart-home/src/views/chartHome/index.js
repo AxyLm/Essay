5321c4ff0a5d5b79dcadf1bbd90cdcbd37b70be1
wyy <thelmg@163.com>
2021-05-31 18:41:26 +0800

    发送图片

--- myProject/chart-home/src/views/chartHome/index.js
+++ myProject/chart-home/src/views/chartHome/index.js
@@ -2,7 +2,7 @@ import React, {
     Component
 } from 'react';
 import * as mqtt from "mqtt"
-import { Button, Card, Row, Col, Comment, Tooltip, List, Input } from 'antd';
+import { Button, Card, Row, Col, Comment, Tooltip, List, Input, Modal, Image } from 'antd';
 import moment from "moment"
 import "./index.css"
 const { TextArea } = Input;
@@ -21,7 +21,8 @@ function MsgRender(props) {
                                 // actions={item.actions}
                                 author={item.sendAccount}
                                 // avatar={item.avatar}
-                                content={item.content + " " + (item._id.indexOf("before") > -1 ? "发送中" : "已发送")}
+                                content={<MsgContent item={item}></MsgContent>}
+                                
                                 datetime={item.sendTime}
                             />
                         </li>
@@ -36,7 +37,19 @@ function MsgRender(props) {
         )
     }
 }
-
+function MsgContent(props) {
+    const { msgType, content, _id } = props.item
+    if (msgType == "3") {
+        return (<div>
+            <Image src={ content}/>
+        </div> )
+    }else{
+        return (<div>
+            {content + " " + (_id.indexOf("before") > -1 ? "发送中" : "已发送")}
+        </div> )
+    }
+    
+}
 class Chart extends Component {
     constructor(props) {
         super(props)
@@ -45,7 +58,10 @@ class Chart extends Component {
             msgList: [],
             content: "",
             connect: null,
-            sendAccount: "rts"
+            sendAccount: "rts",
+            visible: false,
+            modalImgUrl: null,
+            modalBlob:null
         }
     }
     componentDidMount = () => {
@@ -114,19 +130,26 @@ class Chart extends Component {
     onChange = ({ target: { value } }) => {
         this.setState({ content: value });
     };
-    usendMsg = () => {
-        var { content, msgList, sendAccount, msgList_bef } = this.state
+    usendMsg = (type) => {
+        var { content, msgList, sendAccount, msgList_bef, modalImgUrl } = this.state
         const id = ("before" + (new Date()).getTime()).toString()
+
         let msgData = {
             "_id": id,
-            "content": content,
+            "content": null,
             "msgType": "2",
             "sendAccount": sendAccount,
             "sendTime": moment(new Date()).format("YYYY-MM-DD HH:mm:ss"),
         }
+        if (type == "image") {
+            msgData.content = modalImgUrl
+            msgData.msgType = "3"
+        } else {
+            msgData.content = content
+        }
         console.log(JSON.stringify(msgData))
         try {
-            if (content) {
+            if (msgData.content) {
                 this.setState({ content: "" })
                 // 更新本人消息 状态为before
                 msgList_bef.push(msgData)
@@ -151,10 +174,67 @@ class Chart extends Component {
             console.error(error);
         }
     }
+    imgRender = (item) => {
+        console.log(item)
+        const _this = this
+        if (item.type.indexOf("image") > -1) {
+            console.info(item);
+            var blob = item.getAsFile()
+            console.log(blob);
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                _this.setState({
+                    modalBlob:blob,
+                    modalImgUrl:e.target.result,
+                    visible:true
+                })
+            };
+            reader.readAsDataURL(blob);
+
+        } else if (item.kind === "string") {
+            item.getAsString((str) => {
+                // str 是获取到的字符串
+                console.info(str);
+                this.setState({
+                    content: str
+                })
+            })
+        }
+    }
+    onPaste = (e) => {
+
+        console.log(e);
+        var clipboardData = e.clipboardData,//谷歌
+            i = 0,
+            items, item, types;
+        console.log('0')
+
+        if (clipboardData) {
+            console.log('1')
+            items = clipboardData.items;
+            console.log(items)
+            if (items && items.length) {
+                for (let i = 0; i < items.length; i++) {
+                    this.imgRender(items[i]);
+                }
+            }
+        }
+    }
+    handleOk = () => {
+        this.usendMsg("image")
+        this.setState({
+            visible:false
+        })
+    }
+    handleCancel = () => {
+        this.setState({
+            visible:false
+        })
+    }
     render() {
-        const { msgList, content, msgList_bef } = this.state
+        const { msgList, content, msgList_bef, visible, modalImgUrl } = this.state
         return (
-            <div className="App">
+            <div className="App" onPaste={this.onPaste}>
                 <Row gutter={[16, 16]} justify="center">
                     <Col xs={24} sm={20} xl={12}>
                         <Card className="card-u">
@@ -168,6 +248,11 @@ class Chart extends Component {
                         </Card>
                     </Col>
                 </Row>
+                <Modal title="发送图片" okText="确认" cancelText="取消" visible={visible} onOk={this.handleOk} onCancel={this.handleCancel}>
+                    <Image src={modalImgUrl}/>
+                </Modal>
+                <div class="box" contenteditable="true" id="testInput">
+                </div>
             </div>
         )
     }
